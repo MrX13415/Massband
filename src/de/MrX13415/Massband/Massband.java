311cd11e3c439b1cb91350e91d5ad78d353b2e3b
MrX13415 <Mr.X.13415@googlemail.com>
2012-02-14 19:18:23 +0100

    changed licence to CC BY-NC-SA 3.0
    Signed-off-by: MrX13415 <Mr.X.13415@googlemail.com>

--- src/de/MrX13415/Massband/Massband.java
+++ src/de/MrX13415/Massband/Massband.java
@@ -26,18 +26,16 @@ import org.bukkit.plugin.Plugin;
  * @version 2.6.8 r57
  * @author MrX13415
  * 
- * Copyright (C) 2011 MrX13415
- *
- *  This program is free software; you can redistribute it and/or modify it under
- *  the terms of the GNU General Public License as published by the Free Software Foundation;
- *  either version 3 of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along with this program;
- *  if not, see <http://www.gnu.org/licenses/>.
+ * Website:      http://dev.bukkit.org/server-mods/massband/
+ * 
+ * Copyright (C) 2012 MrX13415
+ * 
+ * License:
+ *  
+ *     CreativeCommons Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
+ * 
+ *     For more Informations:
+ *     http://creativecommons.org/licenses/by-nc-sa/3.0/
  */
 
 public class Massband extends JavaPlugin {
