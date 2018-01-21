79ff06770eda3cecec8d50b641ba5b751cfdee89
MrX13415 <mr.x.13415@gmail.com>
2018-01-21 15:53:30 +0100

    Added Permission Visibility option.

--- Massband 3/src/net/icelane/massband/config/configs/Config.java
+++ Massband 3/src/net/icelane/massband/config/configs/Config.java
@@ -4,7 +4,9 @@ import net.icelane.massband.config.ConfigBase;
 import net.icelane.massband.config.Entry;
 import net.icelane.massband.config.EntryTypes.Entry_Boolean;
 import net.icelane.massband.config.EntryTypes.Entry_Double;
+import net.icelane.massband.config.EntryTypes.Entry_Enum;
 import net.icelane.massband.config.EntryTypes.Entry_Long;
+import net.icelane.massband.io.CommandText;
 
 public class Config extends ConfigBase<Config> {
 
@@ -25,12 +27,23 @@ public class Config extends ConfigBase<Config> {
 		return "config.yml";
 	}
 
-	public Entry_Boolean cleanupEnabled = Entry.define("cleanup.enabled", true, "This will remove any leftover \"markers\" on server startup.");
+	public Entry_Boolean cleanupEnabled = 
+			Entry.define("Cleanup.Enabled", true, "This will remove any leftover \"markers\" on server startup.");
 	
-	public Entry_Double defaultEntityLineOffset = Entry.define("marker.LineOffset", 0.3, "The space between multiple lines of a marker.");	
+	public Entry_Double defaultEntityLineOffset = 
+			Entry.define("Marker.LineOffset", 0.3, "The space between multiple lines of a marker.");	
 	
-	public Entry_Boolean marker_showOwnerTags     = Entry.define("marker.OwnerTags.Enable", true, "Shows the name of the player a marker belongs to.");
-	public Entry_Long defaultOwnerHideTicks = Entry.define("marker.OwnerTags.HideTicks", 20L * 3, "ticks (20 tick => 1 sec)");
-	public Entry_Long defaultOwnerShowDelayTicks = Entry.define("marker.OwnerTags.DelayTicks", 10L, "ticks (1 tick => 50 ms)");
+	public Entry_Boolean marker_showOwnerTags = 
+			Entry.define("Marker.OwnerTags.Enable", true, "Shows the name of the player a marker belongs to.");
+	public Entry_Long defaultOwnerHideTicks = 
+			Entry.define("Marker.OwnerTags.HideTicks", 20L * 3, "ticks (20 tick => 1 sec)");
+	public Entry_Long defaultOwnerShowDelayTicks = 
+			Entry.define("Marker.OwnerTags.DelayTicks", 10L, "ticks (1 tick => 50 ms)");
+	
+	public Entry_Enum<CommandText.PermissionVisibility> help_permissionVisibility = 
+			Entry.define("Help.PermissionVisibility",
+					CommandText.PermissionVisibility.class,
+					CommandText.PermissionVisibility.OP,
+					"Who should be able to see the permission in the help message.");
 
 }
